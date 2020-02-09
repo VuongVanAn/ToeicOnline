6125c7ee62aae8e869614324da4206ab2b55b491
VuongVanAn <57897163+VuongVanAn@users.noreply.github.com>
2020-02-09 14:43:41 +0700

    init first project

---
+++ toeic-core-common/src/main/java/vn/myclass/core/common/utils/HibernateUtil.java
@@ -0,0 +1,26 @@
+package vn.myclass.core.common.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        }catch(Throwable e) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
