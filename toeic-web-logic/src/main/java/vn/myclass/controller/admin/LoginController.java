0767cc07252923fe8f9c088338df78b7691a53d2
VuongVanAn <57897163+VuongVanAn@users.noreply.github.com>
2020-02-10 08:27:57 +0700

    init bean util

---
+++ toeic-web-logic/src/main/java/vn/myclass/controller/admin/LoginController.java
@@ -0,0 +1,8 @@
+package vn.myclass.controller.admin;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"login.html"})
+public class LoginController extends HttpServlet {
+}
